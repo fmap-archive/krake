3e3bb999bf4170d2190a04de13d853626ba9e74c
vi <noreply@example.org>
2013-11-06 19:38:18 +0800

    tests for url normaliser

--- tests/unit/definition_spec.js
+++ tests/unit/definition_spec.js
@@ -54,3 +54,26 @@ describe("definition#unpattern()", function() {
   });
 });
 
+describe("definition#normalise", function() {
+  _.each(fixtures, function(fixture) {
+    var normalised = definition.normalise(fixture);
+    it("Should return a list of tasks with string url properties", function() {
+      expect(true).toBe(true);
+      expect(normalised.constructor).toEqual(Array);
+    });
+    it("Said tasks should have urls constructed from permutations of 'non-pattern' properties substituted into 'pattern'..", function() {
+      var permutations = [
+         "https://www.flickr.com/search/?q=kitten&l=comm",
+         "https://www.flickr.com/search/?q=kitten&l=deriv",
+         "https://www.flickr.com/search/?q=cat&l=comm",
+         "https://www.flickr.com/search/?q=cat&l=deriv",
+         "https://www.flickr.com/search/?q=meow&l=comm",
+         "https://www.flickr.com/search/?q=meow&l=deriv",
+        ];
+      _.each(permutations, function(url) {
+        var first = _.findWhere(normalised, { url: url });
+        expect(first).not.toBe(undefined);
+      });
+    });
+  });
+});
