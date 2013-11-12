b80148d6121b9b0fd55ee7bd9c59449459a67182
vi <noreply@example.org>
2013-11-12 18:55:27 +0800

    emitter tests

---
+++ tests/unit/krake_spec.js
@@ -0,0 +1,25 @@
+var root       = __dirname + '/../../';
+var Krake      = require(root + 'lib/krake');
+var cols       = require(root + 'lib/krake/cols');
+var fixtures   = require(root + 'tests/helpers/fixtures');
+var typecheck  = require(root + 'lib/typecheck');
+
+describe("Krake#scrape", function() {
+  beforeEach(function() {
+    spyOn(cols, 'get').andCallFake(function(i, callback) {
+      callback(false, false, fixtures.kitten);
+    });
+  });
+  it("Should return an emitter.", function(){
+    var emitter = new Krake().scrape(fixtures.simple_with_fn);
+    var type = typecheck.type(emitter);
+    expect(type).toEqual("EventEmitter");
+  });
+  it("Should send 'retrieved' messages to the emitter when results have been retrieved, with an object as message.", function(done) {
+    var emitter = new Krake().scrape(fixtures.simple_with_fn);
+    emitter.addListener('retrieved', function(res) {
+      expect(res.constructor).toEqual(Object);
+      done(); // Test first received.
+    });
+  }, 20*1000);
+});
