660b4a36c38bd0483e52fee9fb38c08082475a4f
vi <noreply@example.org>
2013-11-12 16:59:23 +0800

    zip retrieved nodes into table structure

--- tests/unit/cols_spec.js
+++ tests/unit/cols_spec.js
@@ -3,6 +3,59 @@ var cols       = require(root + 'lib/krake/cols');
 var fixtures   = require(root + 'tests/helpers/fixtures');
 var _          = require('underscore');
 
+describe("cols#zip", function() {
+  var task = fixtures.simple;
+  var taskKeys = ["title", "image", "owner", "page"];
+  var someZipped = [ 
+    { title: 'kitten hug',
+      image: 'https://s.yimg.com/pw/images/spaceball.gif',
+      owner: 'rodrigotrovao',
+      page: '/photos/rodrigotrovao/4245998404/' 
+    },
+    { title: 'Kitten!',
+      image: 'https://s.yimg.com/pw/images/spaceball.gif',
+      owner: 'adamjseidl',
+      page: '/photos/ajseidl/4981316470/' 
+    },
+    { title: 'Kittens!',
+      image: 'https://s.yimg.com/pw/images/spaceball.gif',
+      owner: 'beer_squirrel',
+      page: '/photos/beersquirrel/349671331/' 
+    } 
+  ];
+  it("should return a list of objects..", function(done) {
+    cols.cols(task, function(f) {
+      var zipped = cols.zip(f.cols);
+      expect(zipped.constructor).toEqual(Array);
+      _.each(zipped, function(rec) {
+        expect(rec.constructor).toEqual(Object);
+      });
+      done();
+    });
+  }, 20*1000);
+  it("each object including each of the task's keys..", function(done) {
+    cols.cols(task, function(f) {
+      var zipped = cols.zip(f.cols);
+      _.each(zipped, function(rec) {
+        var keys = _.keys(rec);
+        _.each(taskKeys, function(key) {
+          expect(keys).toContain(key);
+        });
+      });
+      done();
+    });
+  }, 20*1000);
+  it("the values being the page's processed, retrieved data..", function(done) {
+    cols.cols(task, function(f) {
+      var zipped = cols.zip(f.cols);
+      _.each(someZipped, function(e) {
+        expect(zipped).toContain(e);
+      });
+      done();
+    });
+  }, 20*1000);
+});
+
 describe("cols#cols", function() {
   var someTitles;
   beforeEach(function(){
