509d3f9c759d3e5c6424560e89a3ba1bc6807bda
vi <noreply@example.org>
2013-11-21 02:49:41 +0800

    write cols function expectations in code

---
+++ tests/unit/expect_spec.js
@@ -0,0 +1,20 @@
+var r = __dirname + '/../../';
+var e = require(r + 'lib/krake/expect');
+var f = require('../helpers/fixtures');
+
+describe("expect", function() {
+  stringNodes = { cols: [{nodes:["str"]}] };
+  objectNodes = { cols: [{nodes:[{some:"obj"}]}] };
+  it("expect.stringNodes", function() {
+    expect(e.stringNodes(stringNodes)).toEqual(true);
+    expect(e.stringNodes(objectNodes).constructor).toEqual(Error);
+  });
+  it("expect.objectNodes", function() {
+    expect(e.objectNodes(objectNodes)).toEqual(true);
+    expect(e.objectNodes(stringNodes).constructor).toEqual(Error);
+  });
+  it("expect.normalised", function() {
+    expect(e.normalised(f.simple)).toEqual(true);
+    expect(e.normalised(f.list_url).constructor).toEqual(Error);
+  });
+});
