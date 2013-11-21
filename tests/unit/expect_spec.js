c65ec6e10a1e80294c040cfb5ca89e66a8644fb6
vi <noreply@example.org>
2013-11-21 11:48:00 +0800

    added expectations for url functions

--- tests/unit/expect_spec.js
+++ tests/unit/expect_spec.js
@@ -17,4 +17,12 @@ describe("expect", function() {
     expect(e.normalised(f.simple)).toEqual(true);
     expect(e.normalised(f.list_url).constructor).toEqual(Error);
   });
+  it("expect.isList", function() {
+    expect(e.isList([])).toEqual(true);
+    expect(e.isList(23).constructor).toEqual(Error);
+  });
+  it("expect.isObject", function() {
+    expect(e.isList({})).toEqual(true);
+    expect(e.isList(23).constructor).toEqual(Error);
+  });
 });
