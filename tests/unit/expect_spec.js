61061c10850a5f071c1be0cfa6bbe13fe6b37031
vi <noreply@example.org>
2013-11-24 01:06:33 +0800

    refactored expect

--- tests/unit/expect_spec.js
+++ tests/unit/expect_spec.js
@@ -5,24 +5,24 @@ var f = require('../helpers/fixtures');
 describe("expect", function() {
   stringNodes = { cols: [{nodes:["str"]}] };
   objectNodes = { cols: [{nodes:[{some:"obj"}]}] };
-  it("expect.stringNodes", function() {
-    expect(e.stringNodes(stringNodes)).toEqual(true);
-    expect(e.stringNodes(objectNodes).constructor).toEqual(Error);
+  it("expect.hasStringNodes", function() {
+    expect(e.hasStringNodes(stringNodes)).toEqual(true);
+    expect(e.hasStringNodes(objectNodes).constructor).toEqual(Error);
   });
-  it("expect.objectNodes", function() {
-    expect(e.objectNodes(objectNodes)).toEqual(true);
-    expect(e.objectNodes(stringNodes).constructor).toEqual(Error);
+  it("expect.hasObjectNodes", function() {
+    expect(e.hasObjectNodes(objectNodes)).toEqual(true);
+    expect(e.hasObjectNodes(stringNodes).constructor).toEqual(Error);
   });
-  it("expect.normalised", function() {
-    expect(e.normalised(f.simple)).toEqual(true);
-    expect(e.normalised(f.list_url).constructor).toEqual(Error);
+  it("expect.isNormalised", function() {
+    expect(e.isNormalised(f.simple)).toEqual(true);
+    expect(e.isNormalised(f.list_url).constructor).toEqual(Error);
   });
   it("expect.isList", function() {
     expect(e.isList([])).toEqual(true);
     expect(e.isList(23).constructor).toEqual(Error);
   });
-  it("expect.isObject", function() {
-    expect(e.isList({})).toEqual(true);
-    expect(e.isList(23).constructor).toEqual(Error);
+  it("isObject", function() {
+    expect(e.isObject({})).toEqual(true);
+    expect(e.isObject(23).constructor).toEqual(Error);
   });
 });
