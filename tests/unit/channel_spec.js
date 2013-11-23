00516a567d8cb9a3109563a40d84367c411c7bc9
vi <noreply@example.org>
2013-11-24 03:07:48 +0800

    separated out channel code

---
+++ tests/unit/channel_spec.js
@@ -0,0 +1,32 @@
+var root         = __dirname + '/../../';
+var cols         = require(root + 'lib/krake/cols');
+var channel      = require(root + 'lib/krake/channel');
+var EventEmitter = require('events').EventEmitter;
+
+describe("channel", function() {
+  beforeEach(function() {
+    ['cols','zip'].forEach(function(f) {
+      spyOn(cols,f).andCallFake(function(task, callback) {
+        callback(42);
+      });
+    });
+  });
+  it("should export a function", function() {
+    expect(channel.constructor).toEqual(Function);
+  });
+  it("should send a 'retrieved' event to the emitter with the result of zip", function(done) {
+    var emitter = new EventEmitter();
+    emitter.on('retrieved', function(res) {
+      expect(res).toEqual(42);
+      done();      
+    });
+    channel([""], emitter);
+  });
+  it("should send a 'completed' event at some point", function(done) {
+    var emitter = new EventEmitter();
+    emitter.on('completed', function() {
+      done();      
+    });
+    channel([""], emitter);
+  });
+});
--- tests/unit/krake_spec.js
+++ tests/unit/krake_spec.js
@@ -1,34 +1,16 @@
 var root      = __dirname + '/../../';
 var Krake     = require(root + 'lib/krake');
 var normalise = require(root + 'lib/krake/normalise');
-var cols      = require(root + 'lib/krake/cols');
+var channel   = require(root + 'lib/krake/channel');
 var fixtures  = require(root + 'tests/helpers/fixtures');
 var events    = require('events');
 
 describe("Krake#scrape", function() {
-  beforeEach(function() {
-    spyOn(cols, 'get').andCallFake(function(i, callback) {
-      callback(false, false, fixtures.kitten);
-    });
-  });
   it("Should return an emitter.", function(){
     var emitter = new Krake().scrape(fixtures.simple_with_fn);
     expect(emitter.constructor).toEqual(events.EventEmitter);
   });
-  it("Should send 'retrieved' messages to the emitter when results have been retrieved, with an object as message.", function(done) {
-    var emitter = new Krake().scrape(fixtures.simple_with_fn);
-    emitter.addListener('retrieved', function(res) {
-      expect(res.constructor).toEqual(Object);
-      done(); // Test first received.
-    });
-  }, 2e4);
-  it("Should send a 'completed' message to the emitter once all results have been retrieved", function(done) {
-    var emitter = new Krake().scrape(fixtures.simple_with_fn);
-    emitter.addListener('completed', function() {
-      done(); // On fail, will hang indefinitely..? TODO
-    });
-  }, 2e4);
-  it("Should send each subtask to createChannels.", function() {
+  xit("Should send each subtask to createChannels.", function() {
     spyOn(cols, 'createChannels');
     var emitter = new Krake().scrape(fixtures.list_url);
     var normalised = normalise(fixtures.list_url);
